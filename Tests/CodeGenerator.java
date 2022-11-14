414acef3afcf1ac14cd2956be7fd4222a246d898
Hannan <56102200+Hannan201@users.noreply.github.com>
2022-11-13 20:31:54 -0500

    Added more helper methods to generate random codes.

--- Tests/CodeGenerator.java
+++ Tests/CodeGenerator.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CodeGenerator {
-
     private static final String ALPHANUMERIC = "123456789abcdefghijklmnopqrstuvwxyz";
     private static final String NUMERIC = "1234567890";
 
@@ -9,12 +10,28 @@ public class CodeGenerator {
         Random random = new Random();
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i <= length; i++) {
+        for (int i = 0; i < length; i++) {
             int index = random.nextInt(text.length());
             result.append(text.charAt(index));
         }
 
         return result.toString();
+    }
+
+    public static List<String> generateCodesNumbers(int count, int length, String delimiter) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add(generateCodeNumbers(length, delimiter));
+        }
+        return list;
+    }
+
+    public static List<String> generateCodesAlphanumeric(int count, int length, String delimiter) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add(generateCodeAlphaNumeric(length, delimiter));
+        }
+        return list;
     }
 
     public static String generateCodeNumbers(int length, String delimiter) {
