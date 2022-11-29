c5518830a79caff205c5bbdbc8d3fcb18fd08847
alkoran5 <fares.alkorani@mail.utoronto.ca>
2022-11-29 16:45:22 -0500

    [DEV-1.5] Completed the UI for the HomePageView and stored the fonts that will be used in this program under resources/fonts. Began the HomePageController as well, which implements functionality for the HomePageView.

---
+++ src/main/java/controllers/HomePageController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomePageController {
+
+    public Button signIn;
+    public Button signUp;
+
+    @FXML
+//    private <PopUp> popup
+
+    public void handleSignIn(ActionEvent e) {
+
+        // open pop up
+        System.out.println("Signed in.");
+    }
+
+    public void handleSignUp(ActionEvent e) {
+
+        System.out.println("Signed up.");}
+
+}
