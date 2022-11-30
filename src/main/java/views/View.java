94c5c65d17b846cf552c817385d5b64494cc6a72
Hannan <hannan.syed@mail.utoronto.ca>
2022-11-29 19:07:30 -0500

    Adjusted the View.java class to now hold the scene being used to display the view. Added a getter and setter method for it as well.

--- src/main/java/views/View.java
+++ src/main/java/views/View.java
@@ -1,6 +1,7 @@
 package views;
 
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 /**
  * This class is responsible for displaying a specific
@@ -8,6 +9,34 @@ import javafx.scene.Parent;
  */
 
 public abstract class View {
+
+    // A reference to store the Scene
+    // object being used to display
+    // this view.
+    private Scene scene;
+
+    /**
+     * Set the scene object being used
+     * to display this view.
+     *
+     * @param newScene The scene object
+     *                 being used to
+     *                 display this view.
+     */
+    public void setScene(Scene newScene) {
+        this.scene = newScene;
+    }
+
+    /**
+     * Get the scene being used to display
+     * this view.
+     *
+     * @return The scene being used to display
+     * this view.
+     */
+    public Scene getScene() {
+        return this.scene;
+    }
 
     /**
      * Initialise the UI elements for this view.
