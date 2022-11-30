62226cef78b663ee2778438d3bbed320c705f0a8
Hannan <56102200+Hannan201@users.noreply.github.com>
2022-11-30 02:52:17 -0500

    Fixed error in View.loadRoot.

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -31,5 +31,7 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         // Entry point.
+        stage.setScene(new Scene(SettingsView.getInstance().getRoot()));
+        stage.show();
     }
 }
