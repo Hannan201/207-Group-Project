e32d37f5cfc0f58c5efcc8a9b333a8832d16dc17
Hannan <56102200+Hannan201@users.noreply.github.com>
2022-12-03 06:13:42 -0500

    [DEV-2.4] Added some more basic methods and attributes to Database.java.

--- src/main/java/user/Database.java
+++ src/main/java/user/Database.java
@@ -10,14 +10,59 @@ import java.util.Set;
 public class Database {
 
     // To check if a user's already logged in.
-    private boolean loggedIn;
+    private static boolean loggedIn;
 
     // To prevent duplicate usernames.
-    private Set<String> usernames;
+    private static Set<String> usernames;
 
     // Store current user to load or save data.
-    private User user;
+    private static User user;
 
+    // Source for the usernames, passwords, and
+    // any configurations.
+    private static String userSource;
 
+    // To only allow onc source to be set
+    // for usernames and passwords.
+    private static boolean userSourceSet;
 
+    // Source for the social media accounts
+    // and backup codes for that account.
+    private static String accountsSource;
+
+    // To only allow one source to be
+    // set for the backup codes and
+    // accounts.
+    private static boolean accountSourceSet;
+
+    /**
+     * Sets the source to the CSV file where
+     * the usernames and passwords are stored.
+     *
+     * @param filePath Path to the file.
+     */
+    public static void setUserSource(String filePath) {
+        if (!userSourceSet) {
+            userSource = filePath;
+        }
+    }
+
+    /**
+     * Set the source to the file where
+     * the social media accounts and
+     * backup codes can be found.
+     *
+     * @param filePath Path to the file.
+     */
+    public static void setAccountsSource(String filePath) {
+        if (!accountSourceSet) {
+            accountsSource = filePath;
+        }
+    }
+
+    public static void registerUser(String username, String password) {
+        if (accountSourceSet && userSourceSet && !loggedIn) {
+
+        }
+    }
 }
