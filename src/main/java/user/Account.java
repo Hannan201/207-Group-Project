1c20e8ff25530a2ad98436670be8c7c8334c1463
alkoran5 <fares.alkorani@mail.utoronto.ca>
2022-12-04 00:41:59 -0500

    [DEV-1.5] Removed erroneous import statement from Java Swing to resolve a merge conflict.

--- src/main/java/user/Account.java
+++ src/main/java/user/Account.java
@@ -3,7 +3,6 @@ package user;
 import behaviors.interfaces.ReadCodeBehavior;
 import javafx.scene.image.ImageView;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
