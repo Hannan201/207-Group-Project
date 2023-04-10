1011a4cbb67136d8b5cdecf9b349f3f938b7b65b
Hannan <hannan.syed@mail.utoronto.ca>
2023-04-10 01:29:28 -0400

    Home page is now responsive.

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,14 +1,50 @@
 package controllers;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import views.SignInView;
 import views.SignUpView;
 import views.View;
 
-public class HomePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public Button signIn;
-    public Button signUp;
+public class HomePageController implements Initializable {
+
+    @FXML
+    private Button signIn;
+
+    @FXML
+    private Button signUp;
+
+    @FXML
+    private HBox buttons;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Region space;
+
+    @FXML
+    private BorderPane main;
+
+    @FXML
+    private Region above;
+
+    @FXML
+    private Region below;
+
+    DoubleProperty test = new SimpleDoubleProperty();
+    DoubleProperty factor = new SimpleDoubleProperty();
+
 
     /**
      * A handle method for the Sign-In button that opens a pop-up to allow the
@@ -24,5 +60,31 @@ public class HomePageController {
      */
     public void handleSignUp() {
         View.loadNewWindow(SignUpView.getInstance());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        below.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(buttons.heightProperty()).divide(2).add(17));
+        above.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(buttons.heightProperty()).divide(2).subtract(17));
+        space.prefWidthProperty().bind(title.widthProperty().subtract(signIn.widthProperty()).subtract(signUp.widthProperty()));
+        boolean[] belowLimit = {false};
+
+        main.widthProperty().addListener((observableValue, number, t1) -> {
+            if (t1.doubleValue() < title.getPrefWidth() - 45) {
+                factor.set(title.getPrefWidth() - t1.doubleValue());
+                if (!belowLimit[0]) {
+                    belowLimit[0] = true;
+                    above.prefHeightProperty().bind(test.subtract(factor));
+                    below.prefHeightProperty().bind(test.subtract(factor));
+                }
+            } else {
+                test.set(below.prefHeightProperty().getValue());
+                if (belowLimit[0]) {
+                    belowLimit[0] = false;
+                    below.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(signUp.heightProperty()).divide(2));
+                    above.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(signUp.heightProperty()).divide(2));
+                }
+            }
+        });
     }
 }
