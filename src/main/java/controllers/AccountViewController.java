6b1758e46b7a3b6394477f27311adfb09aea7cee
Hannan <56102200+Hannan201@users.noreply.github.com>
2023-04-15 07:29:21 -0400

    View for displaying all the accounts is now responsive.

--- src/main/java/controllers/AccountViewController.java
+++ src/main/java/controllers/AccountViewController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
 import user.Account;
 import data.Database;
 import user.User;
@@ -18,13 +24,31 @@ import java.util.ResourceBundle;
 public class AccountViewController implements Initializable {
 
     @FXML
+    private BorderPane box;
+
+    @FXML
+    private TextField search;
+
+    @FXML
+    private Region space;
+
+    @FXML
     public ListView<Account> accounts;
+
+    @FXML
+    private Region bottomListView;
+
+    @FXML
+    private Region top;
 
     @FXML
     public Button add;
 
     @FXML
     public Button pin;
+
+    @FXML
+    private Region bottomButtons;
 
     @FXML
     public Label title;
@@ -38,10 +62,43 @@ public class AccountViewController implements Initializable {
     @FXML
     public Button settings;
 
+    private final ObjectProperty<Insets> padding = new SimpleObjectProperty<>(new Insets(40, 5, 0 ,5));
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         accounts.setCellFactory(new AccountCellFactory());
         accounts.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        top.prefHeightProperty().bind(
+                search.heightProperty()
+                        .add(space.heightProperty())
+        );
+
+        bottomButtons.prefHeightProperty().bind(
+                bottomListView.heightProperty()
+        );
+
+        box.paddingProperty().bind(padding);
+
+        box.heightProperty().addListener(((observableValue, oldHeight, newHeight) -> {
+            if (newHeight.doubleValue() < 594) {
+                padding.set(new Insets(Math.max(0, 40 - 594 + newHeight.doubleValue()), 5, 0, 5));
+
+                if (newHeight.doubleValue() < 570) {
+                    BorderPane.setAlignment(title, Pos.CENTER);
+
+                    if (newHeight.doubleValue() < 395) {
+                        space.setMinHeight(Region.USE_COMPUTED_SIZE);
+                    } else {
+                        space.setMinHeight(space.getPrefHeight());
+                    }
+                } else {
+                    BorderPane.setAlignment(title, Pos.BASELINE_CENTER);
+                }
+            } else {
+                padding.set(new Insets(40, 5, 0, 5));
+            }
+        }));
     }
 
     /**
