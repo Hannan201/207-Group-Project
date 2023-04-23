70442b2b5b4196f70468ba706c993e1159b8d827
Hannan <56102200+Hannan201@users.noreply.github.com>
2023-04-23 19:40:50 -0400

    [DEV] Font scales correctly when in full-screen, but not other sizes

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,15 +1,14 @@
 package controllers;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.ObjectExpression;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
 import views.SignInView;
@@ -18,7 +17,6 @@ import views.View;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.Callable;
 
 public class HomePageController implements Initializable {
 
@@ -29,9 +27,6 @@ public class HomePageController implements Initializable {
     private Button signUp;
 
     @FXML
-    private HBox buttons;
-
-    @FXML
     private Label title;
 
     @FXML
@@ -40,15 +35,27 @@ public class HomePageController implements Initializable {
     @FXML
     private BorderPane main;
 
-    @FXML
-    private Region above;
+    private final static int PREF_WIDTH  = 540;
 
-    @FXML
-    private Region below;
+    private final static int PREF_HEIGHT = 620;
 
-    DoubleProperty test = new SimpleDoubleProperty();
-    DoubleProperty factor = new SimpleDoubleProperty();
+    private static final int TITLE_PREF_WIDTH = 207;
+    private static final int TITLE_MAX_WIDTH = 380;
 
+    private static final int BASE_FONT_SIZE = 45;
+    private static final int MAX_FONT_SIZE = 85;
+    private static final int DEFAULT_FONT_SIZE = 15;
+
+    private static final double FONT_RATIO = (double) DEFAULT_FONT_SIZE / BASE_FONT_SIZE;
+
+    // For the title.
+    private final ObjectProperty<Font> titleFontTracking = new SimpleObjectProperty<>(Font.getDefault());
+    private final DoubleProperty titleFontSize = new SimpleDoubleProperty();
+
+    // For the buttons.
+    private final ObjectProperty<Font> buttonFontTracking = new SimpleObjectProperty<>(Font.font("Montserrat-Regular"));
+
+    private DoubleProperty test = new SimpleDoubleProperty();
 
     /**
      * A handle method for the Sign-In button that opens a pop-up to allow the
@@ -66,34 +73,56 @@ public class HomePageController implements Initializable {
         View.loadNewWindow(SignUpView.getInstance());
     }
 
-    private ObjectExpression<Font> fontTracking;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        fontTracking = Bindings.createObjectBinding(() -> Font.font("Montserrat", (((main.getWidth() + main.getHeight()) / (2000.0)) * 100.0) * (45.0 / 58.0)), main.widthProperty(), main.heightProperty());
-        title.fontProperty().bind(fontTracking);
+        titleFontSize.bind(main.widthProperty().add(main.heightProperty())
+                .divide(1280 + 720)
+                .multiply(100)
+                .multiply(45.0 / 58.0)
+        );
 
-        below.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(buttons.heightProperty()).divide(2).add(17));
-        above.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(buttons.heightProperty()).divide(2).subtract(17));
-        space.prefWidthProperty().bind(title.widthProperty().subtract(signIn.widthProperty()).subtract(signUp.widthProperty()));
-        boolean[] belowLimit = {false};
+        main.widthProperty().addListener(((observableValue, oldWidth, newWidth) -> {
+            double result = Math.min(MAX_FONT_SIZE, titleFontSize.doubleValue());
+            titleFontTracking.set(Font.font(result));
+            buttonFontTracking.set(Font.font("Montserrat-Regular", result * FONT_RATIO));
+        }));
 
-        main.widthProperty().addListener((observableValue, number, t1) -> {
-            if (t1.doubleValue() < title.getPrefWidth() - 45) {
-                factor.set(title.getPrefWidth() - t1.doubleValue());
-                if (!belowLimit[0]) {
-                    belowLimit[0] = true;
-                    above.prefHeightProperty().bind(test.subtract(factor));
-                    below.prefHeightProperty().bind(test.subtract(factor));
-                }
+        main.heightProperty().addListener(((observableValue, oldHeight, newHeight) -> {
+            double result = Math.min(MAX_FONT_SIZE, titleFontSize.doubleValue());
+            titleFontTracking.set(Font.font(result));
+            buttonFontTracking.set(Font.font("Montserrat-Regular", result * FONT_RATIO));
+        }));
+
+        title.fontProperty().bind(titleFontTracking);
+        signUp.fontProperty().bind(buttonFontTracking);
+        signIn.fontProperty().bind(buttonFontTracking);
+        title.prefWidthProperty().bind(test);
+
+        main.widthProperty().addListener(((observableValue, oldWidth, newWidth) -> {
+            if (newWidth.doubleValue() > PREF_WIDTH) {
+                double delta = (newWidth.doubleValue() - PREF_WIDTH) / 6.0;
+                test.set(Math.min(TITLE_MAX_WIDTH, TITLE_PREF_WIDTH + delta));
+            } else if (newWidth.doubleValue() < PREF_WIDTH) {
+                double delta = (PREF_WIDTH - newWidth.doubleValue()) / 3.0;
+                test.set(TITLE_PREF_WIDTH - delta);
             } else {
-                test.set(below.prefHeightProperty().getValue());
-                if (belowLimit[0]) {
-                    belowLimit[0] = false;
-                    below.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(signUp.heightProperty()).divide(2));
-                    above.prefHeightProperty().bind(main.heightProperty().subtract(title.prefHeightProperty()).subtract(signUp.heightProperty()).divide(2));
-                }
+                test.set(TITLE_PREF_WIDTH);
             }
-        });
+        }));
+
+        main.heightProperty().addListener(((observableValue, oldHeight, newHeight) -> {
+            if (newHeight.doubleValue() > PREF_HEIGHT) {
+                double delta = (newHeight.doubleValue() - PREF_HEIGHT) / 5.0;
+                test.set(Math.min(TITLE_MAX_WIDTH, test.getValue() + delta));
+            } else if (newHeight.doubleValue() < PREF_HEIGHT) {
+                double delta = (PREF_HEIGHT - newHeight.doubleValue()) / 5.0;
+                test.set(test.getValue() - delta);
+            } else {
+                test.set(TITLE_PREF_WIDTH);
+            }
+        }));
+
+
+        space.prefWidthProperty().bind(title.widthProperty().subtract(signIn.widthProperty()).subtract(signUp.widthProperty()));
     }
 }
