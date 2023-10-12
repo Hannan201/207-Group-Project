ce9ce04c05194355398cfee71b7a6ffe2dedfbdd
Hannan <hannan.syed@mail.utoronto.ca>
2023-10-12 04:31:30 -0400

    [DEV] Update samples class with documentation and logger.

--- app/src/test/java/cypher/enforcers/Samples.java
+++ app/src/test/java/cypher/enforcers/Samples.java
@@ -1,18 +1,27 @@
 package cypher.enforcers;
 
 import cypher.enforcers.utilities.Utilities;
+import cypher.enforcers.views.themes.Theme;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
+/*
+ This classes contains the paths to the sample text files based on the
+ social media platform. Mainly here for easy testing.
+ */
 public enum Samples {
 
+    // Path to platforms.
     DISCORD("discord"),
     GITHUB("github"),
     GOOGLE("google"),
     SHOPIFY("shopify");
 
-    private final String path;
+    // Holds the path to the file.
+    private String path;
 
     Samples(String name) {
         try {
@@ -22,10 +31,20 @@ public enum Samples {
                     ).toURI()
             ).toString();
         } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+            Logger logger = LoggerFactory.getLogger(Theme.class);
+            logger.error(e, () ->
+                    "Failed to load file from test resources /cypher/enforcers/Sample Text Files/" +
+                    name +
+                    "_codes_sample.txt. Cause: "
+            );
         }
     }
 
+    /**
+     * Return the path to the file.
+     *
+     * @return Path to the file as a string.
+     */
     public String getPath() {
         return this.path;
     }
