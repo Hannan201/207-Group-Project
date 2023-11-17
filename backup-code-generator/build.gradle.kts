eebb5a4a32e53c5b3817a8f014ddf3bf4c8c5d75
Hannan <hannan.syed@mail.utoronto.ca>
2023-11-16 21:51:36 -0500

    [DEV] First attempt at fixing task dependency issue.

--- backup-code-generator/build.gradle.kts
+++ backup-code-generator/build.gradle.kts
@@ -411,7 +411,6 @@ tasks.register<Checksum>("generateAppChecksums") {
 // M1 processor.
 tasks.register<Checksum>("generateM1Checksums") {
     inputFiles.from(m1UberJarTask.get().outputs.files)
-    dependsOn(tasks.getByPath(":backup-code-generator:jpackageM1").path)
     inputFiles.from(
         layout.buildDirectory.dir("jpackage-m1")
             .get().asFileTree
